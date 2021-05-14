d1b6c500fc5f2ecbaaad832261b64ff47606e9d0
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-14 13:30:42 +0300

    Auto-generated commit

--- src/app/strikethrough.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { StrikethroughDirective } from './strikethrough.directive';
-
-describe('StrickthroughDirective', () => {
-  it('should create an instance', () => {
-    const directive = new StrikethroughDirective();
-    expect(directive).toBeTruthy();
-  });
-});
