3bf73a9d6a5ddc04ea91ec86f062fddd1633359d
nancy88199488 <nancy.moranga@student.moringaschool.com>
2021-05-14 13:18:00 +0300

    add the files

---
+++ src/app/strikethrough.directive.spec.ts
@@ -0,0 +1,8 @@
+import { StrikethroughDirective } from './strikethrough.directive';
+
+describe('StrickthroughDirective', () => {
+  it('should create an instance', () => {
+    const directive = new StrikethroughDirective();
+    expect(directive).toBeTruthy();
+  });
+});
